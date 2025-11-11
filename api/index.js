bd7b88db878c8ded35dba02d84c1d430afb34e56
Micco <miccoasigo@gmail.com>
2025-11-11 21:15:33 +0200

    testing middleware

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ import productRouter from "./routers/product_router.js";
 const app = express();
 
 app.use(express.json());
+app.use((req,res,next)=>{
+    res.send("trying")
+})
 app.use("/products",productRouter)
 
 const handler = severless(app);
