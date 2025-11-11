5bacdc9be59febf9b6228e707c21019830b75fda
Micco <miccoasigo@gmail.com>
2025-11-11 18:08:36 +0200

    init commit

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import severless from "serverless-http";
+import productRouter from "./routers/product_router.js";
+const app = express();
+
+app.use(express.json());
+app.use("/products",productRouter)
+
+const handler = severless(app);
+export default handler;
+
+export {app}
+
+
