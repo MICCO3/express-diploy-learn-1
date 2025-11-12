0fe3e16dadee4378d7b91c278a0e80c9b2cbcd5a
Micco <miccoasigo@gmail.com>
2025-11-12 07:31:58 +0200

    index edited

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import severless from "serverless-http";
 import productRouter from "./routers/product_router.js";
+
 const app = express();
 
 app.use(express.json());
-app.use((req,res,next)=>{
-    res.send("trying")
-})
+
+
 app.use("/products",productRouter)
 
- export const handler = severless(app);
+ export default  severless(app);
  
 
 //export {app}
